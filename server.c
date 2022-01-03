76212ee0a498551f75413f4ddddad77752ab0c42
Sponiar1 <91843716+Sponiar1@users.noreply.github.com>
2022-01-03 13:33:47 +0100

    login function + multiclient

--- server.c
+++ server.c
@@ -5,6 +5,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void *userInteraction(int newsockfd) {
+    int n;
+    char buffer[256];
+    char vyzva[50];
+
+    bzero(vyzva, 50);
+    strcpy(vyzva, "Meno: ");
+    n = write(newsockfd, vyzva, strlen(vyzva) + 1);
+    n = read(newsockfd, buffer, 255);
+    char meno[50];
+    bzero(meno, 50);
+    strcpy(meno, buffer);
+    printf("%s \n", meno);
+    int a = 10;
+    bzero(buffer, 256);
+    bzero(vyzva, 50);
+    strcpy(vyzva, "Heslo: ");
+    n = write(newsockfd, vyzva, strlen(vyzva) + 1);
+    n = read(newsockfd, buffer, 255);
+    char heslo[50];
+    bzero(heslo, 50);
+    strcpy(heslo, buffer);
+    printf("%s \n", heslo);
+    int logged = 0;
+    logged = login(meno, heslo);
+    while (logged==1) {
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0) {
+            perror("Error reading from socket");
+            return 4;
+        }
+
+        printf("Here is the message: %s\n", buffer);
+        if (strncmp("end", buffer, 3) == 0) {
+            break;
+        }
+        const char *msg = "I got your message";
+        n = write(newsockfd, msg, strlen(msg) + 1);
+
+        if (n < 0) {
+            perror("Error writing to socket");
+            return 5;
+        }
+    }
+    close(newsockfd);
+}
 
 int main(int argc, char *argv[])
 {
@@ -40,39 +89,28 @@ int main(int argc, char *argv[])
 
     listen(sockfd, 5);
     cli_len = sizeof(cli_addr);
+    bzero(buffer, 256);
 
-    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
-    if (newsockfd < 0)
-    {
-        perror("ERROR on accept");
-        return 3;
+    while(1==1) {
+        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
+        if (newsockfd < 0)
+        {
+            perror("ERROR on accept");
+            return 3;
+        }
+        pthread_t client;
+        pthread_create(&client, NULL, &userInteraction, newsockfd);
     }
-    
-    bzero(buffer, 256);
-    char vyzva[50];
-    bzero(vyzva, 50);
-    strcpy(vyzva, "Meno: ");
-    n = write(newsockfd, vyzva, strlen(vyzva) + 1);
-    n = read(newsockfd, buffer, 255);
-    char meno[50];
-    bzero(meno, 50);
-    strcpy(meno, buffer);
-    printf("%s \n", meno);
+    close(sockfd);
 
-    bzero(buffer, 256);
-    bzero(vyzva, 50);
-    strcpy(vyzva, "Heslo: ");
-    n = write(newsockfd, vyzva, strlen(vyzva) + 1);
-    n = read(newsockfd, buffer, 255);
-    char heslo[50];
-    bzero(heslo, 50);
-    strcpy(heslo, buffer);
-    printf("%s \n", heslo);
+    return 0;
+}
 
+int login(char meno[], char heslo[]) {
     int found;
     found = 0;
     FILE *f;
-    f = fopen("/tmp/PosSemTest/login.txt", "r");
+    f = fopen("/tmp/PosSemTest2/login.txt", "r");
     char fileMeno[50];
     char fileHeslo[50];
     bzero(fileMeno, 50);
@@ -92,36 +130,7 @@ int main(int argc, char *argv[])
     }
 
     fclose(f);
-    if(found == 0) {
-        perror("User not found");
-        return 22;
-    }
-
-    while(1==1) {
-        bzero(buffer, 256);
-        n = read(newsockfd, buffer, 255);
-        if (n < 0) {
-            perror("Error reading from socket");
-            return 4;
-        }
-
-        printf("Here is the message: %s\n", buffer);
-        if(strncmp("end", buffer, 3) == 0) {
-            break;
-        }
-        const char *msg = "I got your message";
-        n = write(newsockfd, msg, strlen(msg) + 1);
-
-        if (n < 0) {
-            perror("Error writing to socket");
-            return 5;
-        }
-
-    }
-    close(newsockfd);
-    close(sockfd);
-
-    return 0;
+    return found;
 }
 
 /*
